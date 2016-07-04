92fd2982797b897798d2408cce65768129505a95
runseb <runseb@gmail.com>
2016-07-04 17:05:39 +0200

    remove kubectl exec and replace with API calls

--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 
@@ -10,45 +9,21 @@ import (
 
 const (
 	slackTokenLabel        string = "KUBEBOT_SLACK_TOKEN"
-	slackChannelsLabel     string = "KUBEBOT_SLACK_CHANNELS_IDS"
-	slackAdminsLabel       string = "KUBEBOT_SLACK_ADMINS_NICKNAMES"
-	slackCommandsLabel     string = "KUBEBOT_SLACK_VALID_COMMANDS"
-	notDefinedErrorMessage string = "%s env variable not defined"
 )
 
 var (
 	kb *Kubebot
 )
 
-func validateEnvParams() error {
-	if os.Getenv(slackTokenLabel) == "" {
-		return errors.New(fmt.Sprintf(notDefinedErrorMessage, slackTokenLabel))
-	}
-	if os.Getenv(slackChannelsLabel) == "" {
-		return errors.New(fmt.Sprintf(notDefinedErrorMessage, slackChannelsLabel))
-	}
-	if os.Getenv(slackAdminsLabel) == "" {
-		return errors.New(fmt.Sprintf(notDefinedErrorMessage, slackAdminsLabel))
-	}
-	if os.Getenv(slackCommandsLabel) == "" {
-		return errors.New(fmt.Sprintf(notDefinedErrorMessage, slackCommandsLabel))
-	}
-
-	return nil
-}
-
 func main() {
 
-	if err := validateEnvParams(); err != nil {
-		fmt.Printf("Kubebot cannot run. Error: %s\n", err.Error())
+	if err := os.Getenv(slackTokenLabel); err == "" {
+		fmt.Printf("Missing Slack Token. \n")
 		return
 	}
 
 	kb = &Kubebot{
 		token:    os.Getenv(slackTokenLabel),
-		admins:   stringToMap(os.Getenv(slackAdminsLabel), " "),
-		channels: stringToMap(os.Getenv(slackChannelsLabel), " "),
-		commands: stringToMap(os.Getenv(slackCommandsLabel), " "),
 	}
 
 	slack.Run(kb.token)
